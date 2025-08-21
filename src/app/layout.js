3e9059282e8eb4d3ca59d52072d5ce6761decc03
Seemab Anas <seemabanas2022@gmail.com>
2025-08-21 15:53:45 +0500

    cookies22

--- src/app/layout.js
+++ src/app/layout.js
@@ -36,7 +36,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.variable} antialiased`}>
-        {/* Google Analytics */}
+        {/* ✅ Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-6LSQ2K35X9"
           strategy="afterInteractive"
@@ -50,6 +50,7 @@ export default function RootLayout({ children }) {
           `}
         </Script>
 
+        {/* App Components */}
         <Navbar />
         {children}
         <WhatsAppChat />
