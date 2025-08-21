bdc62309d9ecbf96cec7232d862e648804e17600
Seemab Anas <seemabanas2022@gmail.com>
2025-08-21 15:21:18 +0500

    cookies

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,6 +26,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-6LSQ2K35X9"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-6LSQ2K35X9');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${inter.variable} antialiased`}
       >
